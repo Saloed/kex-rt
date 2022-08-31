d9eb75f1533e4f1b1ce25c965ec0822310184934
Saloed <napster_1997@mail.ru>
2022-08-31 16:06:23 +0300

    use gradle build

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("org.jetbrains.research.kex-base")
+    java
+}
+
+dependencies {
+    implementation("org.jetbrains.research", "kex-intrinsics", `kex-intrinsics-version`)
+}
+
+description = "kex-rt"
+
+tasks {
+    withType<JavaCompile> {
+        sourceCompatibility = "1.8"
+        targetCompatibility = "1.8"
+    }
+    withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
